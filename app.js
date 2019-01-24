11cf30866e608efe9562aaca000d8377a7d67cb1
maticb <maticb@MATIC10>
2019-01-24 10:53:48 +0100

    a

--- app.js
+++ app.js
@@ -17,4 +17,20 @@ var svxResult = survexService.createSurvexForTopFile(topFileFromStream);
 
 console.log(svxResult);
 
+
 // TODO for collection of files
+
+fs.readdir('./temp/', (err, files) => {
+    console.log(files);
+    files.forEach(file => {
+        var topFile = topService.openTopFromPath('./temp/'+file);
+        console.log(file);
+        topFile.shots.forEach(shot => {
+            if(shot.idFrom != null && shot.idTo != null){
+                
+                
+                console.log(`${shot.idFrom.id}\t${shot.idTo.id}\t${shot.dist}\t${shot.azimuth}\t${shot.incl}`);
+            }
+        });
+    });
+  })
